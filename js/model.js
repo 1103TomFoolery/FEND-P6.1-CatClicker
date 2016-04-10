088cd899d695acb00782a685f334cf29a42466f8
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2016-04-09 19:57:55 -0700

    -w

---
+++ js/model.js
@@ -0,0 +1,8 @@
+var CatData = [
+    {name: "Bill", path: "images/Bill.jpg"  , clickCount: 0},
+    {name: "Pissed" , path: "images/angryCat.jpg" , clickCount: 0},
+    {name: "Playful", path: "images/playful.jpg", clickCount: 0}, 
+    {name: "Cutie", path: "images/cutie.png", clickCount: 0},
+    {name: "Soaked", path: "images/wetCat.jpg", clickCount: 0},
+    {name: "Burrito", path: "images/burrito.jpg", clickCount: 0}
+    ];
